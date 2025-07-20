94fa2b0107779c31974154d439db3cde739ed333
qq775193759 <775193759@qq.com>
2025-07-20 11:25:46 +0800

    ADD format_frame_data

--- mllm_fm/core/format.py
+++ mllm_fm/core/format.py
@@ -8,6 +8,10 @@ def seq_parser(seq_str):
     cleaned_seq_list = list(filter(None, seq_list))
     return seq_list
     
+def variable_parser(variable):
+    variable_name = variable
+    variable_type = 'list'
+    return variable_name, variable_type
 
 class FormatBase:
     def __init__(self, data_format):
@@ -20,8 +24,27 @@ class FormatBase:
             isinstance(value, str):
                 value = seq_parser(value)
             self.data_format[key] = value
+            
+    def format_frame_data(self, data, video_idx, frame_idx):
+        frame_data = {}
+        for role in self.data_format:
+            variables = self.data_format[role]
+            content_list = []
+            for variable in variables:
+                variable_name, variable_type = variable_parser(variable)
+                if variable_type == 'list':
+                    content_data = data[variable_name][video_idx][frame_idx]
+                if variable_type == 'element':
+                    content_data = data[variable_name][video_idx]
+                content_list.append(content_data)
+            frame_data[role] = content_list
+        return frame_data
+        
+    def format_video_data(self, data, video_idx):
+        pass
 
-
+    def format_all_data(self, data):
+        pass
 
 
 
